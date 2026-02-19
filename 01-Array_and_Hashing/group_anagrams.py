83358bcdfc03f50be10416d077771e740c0e74b3
Andy Wong <andywong.devs@gmail.com>
2026-02-19 21:30:08 +1000

    questions

---
+++ 01-Array_and_Hashing/group_anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        my_map = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            if key not in my_map:
+                my_map[key] = []
+            my_map[key].append(s)
+        return list(my_map.values())
+
