f4e75a917e866c8823c3abfc57bcc9f702355e98
Andy Wong <andywong.devs@gmail.com>
2026-02-20 20:17:17 +1000

    sort_an_array. heap sort answer version added

--- 01-Array_and_Hashing/sort_an_array.py
+++ 01-Array_and_Hashing/sort_an_array.py
@@ -69,4 +69,42 @@ class Solution:
         return i
 
 # heap sort
-    
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+
+        for i in range(n // 2 - 1, -1, -1):
+            self._heapify(nums, n, i)
+
+
+        for i in range(n - 1, 0, -1):
+
+            nums[i], nums[0] = nums[0], nums[i]
+
+            self._heapify(nums, i, 0)
+            
+        return nums
+
+    def _heapify(self, nums: List[int], n: int, i: int):
+        
+        while True:
+            largest = i
+            left = 2 * i + 1
+            right = 2 * i + 2
+
+
+            if left < n and nums[left] > nums[largest]:
+                largest = left
+
+           
+            if right < n and nums[right] > nums[largest]:
+                largest = right
+
+
+            if largest == i:
+                break
+
+
+            nums[i], nums[largest] = nums[largest], nums[i]
+            i = largest
+
